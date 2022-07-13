f9ba2b164b7cafe7f8671583bf03e6995736a463
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-07-13 17:16:40 +0200

    Visualizzazione vue in html e chiamata axios + ciclo post

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -10,7 +10,7 @@ class PageController extends Controller
 {
     public function index(){
 
-        $posts = Post::all();
+        $posts = Post::with('category')->with('tags')->get();
 
         return response()->json($posts);
 
---
+++ resources/js/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1>Tutti i Post</h1>
+        <ul>
+
+            <li v-for="post in posts" :key="post.id">
+
+
+                <h1>{{post.title}}</h1>
+                <p>{{post.contenet}}</p>
+            </li>
+
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+name:'App',
+
+    data() {
+        return {
+            posts:null
+        }
+    },
+    mounted() {
+
+        this.getApi()
+
+
+    },
+
+    methods: {
+        getApi(){
+            axios.get('/api/posts')
+            .then(res =>{
+                this.posts=res.data;
+            })
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
