bdd4d96df3160193a27d7f63da8439048525e857
Apollo11-hub <Antonio-Pollo@outlook.it>
2022-07-13 15:57:45 +0200

    Creata api e generato json di posts separato scss in admin e guest(front) aggiornate rotte, webpack e asset nei file home

--- public/js/app.js
+++ public/js/app.js
@@ -49480,10 +49480,21 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
+/***/ "./resources/sass/admin/style.scss":
+/*!*****************************************!*\
+  !*** ./resources/sass/admin/style.scss ***!
+  \*****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/front/style.scss":
+/*!*****************************************!*\
+  !*** ./resources/sass/front/style.scss ***!
+  \*****************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -49492,14 +49503,15 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*******************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/admin/style.scss ./resources/sass/front/style.scss ***!
+  \*******************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\39347\Desktop\BooleanCareersN61\LARAVEL\laravel-many-to-many\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\39347\Desktop\BooleanCareersN61\LARAVEL\laravel-many-to-many\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\39347\Desktop\BooleanCareersN61\LARAVEL\laravel-api\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\39347\Desktop\BooleanCareersN61\LARAVEL\laravel-api\resources\sass\admin\style.scss */"./resources/sass/admin/style.scss");
+module.exports = __webpack_require__(/*! C:\Users\39347\Desktop\BooleanCareersN61\LARAVEL\laravel-api\resources\sass\front\style.scss */"./resources/sass/front/style.scss");
 
 
 /***/ })
